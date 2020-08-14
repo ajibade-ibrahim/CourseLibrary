f9d6c6d70312435d4108e6e3c2c4c44aa85adfe7
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-14 13:20:32 +0100

    Added repository service interface

---
+++ CourseLibrary/CourseLibrary.Application/ICourseLibraryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using CourseLibrary.Domain;
+
+namespace CourseLibrary.Services
+{
+    public interface ICourseLibraryRepository
+    {
+        void AddAuthor(Author author);
+        void AddCourse(Guid authorId, Course course);
+        bool AuthorExists(Guid authorId);
+        void DeleteAuthor(Author author);
+        void DeleteCourse(Course course);
+        Author GetAuthor(Guid authorId);
+        IEnumerable<Author> GetAuthors();
+        IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+        Course GetCourse(Guid authorId, Guid courseId);
+        IEnumerable<Course> GetCourses(Guid authorId);
+        bool Save();
+        void UpdateAuthor(Author author);
+        void UpdateCourse(Course course);
+    }
+}
