e7445ee28f6651bb9f40f9eeb544e3723ee1f7da
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-14 12:23:34 +0100

    Added Domain project with entities

---
+++ CourseLibrary/CourseLibrary.Domain/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.Domain
+{
+    public class Author
+    {
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+
+        [Required]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string MainCategory { get; set; }
+    }
+}
