1408c9068cbcd574af9dbfeabb265699706d7a91
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-18 14:14:23 +0100

    Implemented basic GET methods for Author and Course

---
+++ CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.ResourceParameters;
+using CourseLibrary.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [HttpHead]
+        public IActionResult GetAuthors([FromQuery] AuthorParameters parameters)
+        {
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(_repository.GetAuthors(parameters)));
+        }
+
+        [HttpGet("api/authors/{id}")]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthor(Guid id)
+        {
+            var author = _repository.GetAuthor(id);
+
+            return author == null
+                ? (ActionResult<IEnumerable<AuthorDto>>)NotFound()
+                : Ok(_mapper.Map<AuthorDto>(author));
+        }
+    }
+}
---
+++ CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Models;
+using CourseLibrary.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api/authors/{authorId}/courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET: api/Course
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDto>> Get(Guid authorId)
+        {
+            var author = _repository.GetAuthor(authorId);
+
+            return author == null
+                ? (ActionResult<IEnumerable<CourseDto>>)NotFound()
+                : Ok(_mapper.Map<IEnumerable<CourseDto>>(_repository.GetCourses(authorId)));
+        }
+
+        [HttpGet]
+        [Route("{courseId}")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_repository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var course = _repository.GetCourse(authorId, courseId);
+            return course == null ? (ActionResult<CourseDto>)NotFound() : _mapper.Map<CourseDto>(course);
+        }
+    }
+}
