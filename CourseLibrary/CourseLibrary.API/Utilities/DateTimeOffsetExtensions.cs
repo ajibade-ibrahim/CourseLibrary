7fd36a91ff947a5b1bb1ea2fae28e2c26d5585a2
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-18 14:19:30 +0100

    Added DateTime extension method to get Age from date of birth

---
+++ CourseLibrary/CourseLibrary.API/Utilities/DateTimeOffsetExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CourseLibrary.API.Utilities
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var dateTimeNow = DateTime.UtcNow;
+
+            if (dateTimeOffset.Date > dateTimeNow.Date)
+            {
+                throw new ArgumentException("Invalid argument. Provided date is in the future.");
+            }
+
+            var years = dateTimeNow.Year - dateTimeOffset.Year;
+            return dateTimeOffset.AddYears(years) < dateTimeNow ? years - 1 : years;
+        }
+    }
+}
