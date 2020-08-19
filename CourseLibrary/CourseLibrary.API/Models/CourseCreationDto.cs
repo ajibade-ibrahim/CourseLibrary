13b4e1609554ee2a31e608d34e4e128ee4c55cd5
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-19 10:28:00 +0100

    Implemented data validation with data annotaions and custom validator attribute.

--- CourseLibrary/CourseLibrary.API/Models/CourseCreationDto.cs
+++ CourseLibrary/CourseLibrary.API/Models/CourseCreationDto.cs
@@ -1,8 +1,31 @@
-﻿namespace CourseLibrary.API.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
+
+namespace CourseLibrary.API.Models
 {
-    public class CourseCreationDto
+    [ProhibitedWordsFilter]
+    public class CourseCreationDto : IValidatableObject
     {
+        [Required]
+        [StringLength(1500)]
         public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title.Equals(Description))
+            {
+                yield return new ValidationResult(
+                    "The title should be different from the description.",
+                    new[]
+                    {
+                        "Course"
+                    });
+            }
+        }
     }
 }
---
+++ CourseLibrary/CourseLibrary.API/ValidationAttributes/ProhibitedWordsFilterAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using CourseLibrary.API.Models;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class ProhibitedWordsFilterAttribute : ValidationAttribute
+    {
+        private readonly List<string> _prohibitedWords = new List<string>
+        {
+            "crap",
+            "fudge",
+            "hell",
+            "dive"
+        };
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Invalid data entered.");
+            }
+
+            var courseCreationDto = (CourseCreationDto)value;
+            var inputWords = string.Concat(courseCreationDto.Title, courseCreationDto.Description).Split(" ");
+
+            var containedWords = _prohibitedWords.Where(word => inputWords.Any(input => word.Equals(input, StringComparison.OrdinalIgnoreCase)))
+                .ToList();
+
+            if (containedWords.Any())
+            {
+                return new ValidationResult(
+                    $"Prohibited words found in title/description: {string.Join(',', containedWords)}",
+                    new[]
+                    {
+                        "Course"
+                    });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
