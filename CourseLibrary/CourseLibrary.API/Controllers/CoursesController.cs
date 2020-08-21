8318c4c8669520d722b9627ecf9fbcbeba5e3a8b
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-08-21 07:37:53 +0100

    Executed cleanup to remove unused references and format code

--- CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary/CourseLibrary.API/Controllers/CoursesController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using CourseLibrary.API.Models;
 using CourseLibrary.Domain;
 using CourseLibrary.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -20,39 +17,29 @@ namespace CourseLibrary.API.Controllers
     [ApiController]
     public class CoursesController : ControllerBase
     {
-        private readonly ICourseLibraryRepository _repository;
-        private readonly IMapper _mapper;
-
         public CoursesController(ICourseLibraryRepository repository, IMapper mapper)
         {
             _repository = repository;
             _mapper = mapper;
         }
 
-        [HttpGet(Name = "GetCoursesForAuthor")]
-        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
+        private readonly IMapper _mapper;
+        private readonly ICourseLibraryRepository _repository;
+
+        public override ActionResult ValidationProblem(
+            [ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
         {
-            var author = _repository.GetAuthor(authorId);
-
-            return author == null
-                ? (ActionResult<IEnumerable<CourseDto>>)NotFound()
-                : Ok(_mapper.Map<IEnumerable<CourseDto>>(_repository.GetCourses(authorId)));
-        }
-
-        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
-        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
-        {
-            if (!_repository.AuthorExists(authorId))
-            {
-                return NotFound();
-            }
-
-            var course = _repository.GetCourse(authorId, courseId);
-            return course == null ? (ActionResult<CourseDto>)NotFound() : _mapper.Map<CourseDto>(course);
+            var apiBehaviorOptions = HttpContext.RequestServices
+                .GetRequiredService<IOptions<ApiBehaviorOptions>>()
+                .Value;
+            var result = apiBehaviorOptions.InvalidModelStateResponseFactory(ControllerContext);
+            return (ActionResult)result;
         }
 
         [HttpPost]
-        public ActionResult<CourseDto> CreateCourseForAuthor(Guid authorId, [FromBody] CourseCreationDto courseCreationDto)
+        public ActionResult<CourseDto> CreateCourseForAuthor(
+            Guid authorId,
+            [FromBody] CourseCreationDto courseCreationDto)
         {
             if (!_repository.AuthorExists(authorId))
             {
@@ -78,6 +65,57 @@ namespace CourseLibrary.API.Controllers
                     courseId = courseDto.Id
                 },
                 courseDto);
+        }
+
+        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_repository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var course = _repository.GetCourse(authorId, courseId);
+            return course == null ? (ActionResult<CourseDto>)NotFound() : _mapper.Map<CourseDto>(course);
+        }
+
+        [HttpGet(Name = "GetCoursesForAuthor")]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
+        {
+            var author = _repository.GetAuthor(authorId);
+
+            return author == null
+                ? (ActionResult<IEnumerable<CourseDto>>)NotFound()
+                : Ok(_mapper.Map<IEnumerable<CourseDto>>(_repository.GetCourses(authorId)));
+        }
+
+        [HttpPatch("{courseId}")]
+        public IActionResult Patch(Guid authorId, Guid courseId, JsonPatchDocument<CourseUpdateDto> jsonPatchDocument)
+        {
+            if (!_repository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var course = _repository.GetCourse(authorId, courseId);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var courseUpdateDto = _mapper.Map<CourseUpdateDto>(course);
+            jsonPatchDocument.ApplyTo(courseUpdateDto, ModelState);
+
+            if (TryValidateModel(courseUpdateDto))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(courseUpdateDto, course);
+            _repository.Save();
+
+            return NoContent();
         }
 
         [HttpPut("{courseId}")]
@@ -112,45 +150,6 @@ namespace CourseLibrary.API.Controllers
             _repository.Save();
 
             return NoContent();
-        }
-
-        [HttpPatch("{courseId}")]
-        public IActionResult Patch(Guid authorId, Guid courseId, JsonPatchDocument<CourseUpdateDto> jsonPatchDocument)
-        {
-            if (!_repository.AuthorExists(authorId))
-            {
-                return NotFound();
-            }
-
-            var course = _repository.GetCourse(authorId, courseId);
-
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            var courseUpdateDto = _mapper.Map<CourseUpdateDto>(course);
-            jsonPatchDocument.ApplyTo(courseUpdateDto, ModelState);
-
-            if (TryValidateModel(courseUpdateDto))
-            {
-                return ValidationProblem(ModelState);
-            }
-
-            _mapper.Map(courseUpdateDto, course);
-            _repository.Save();
-
-            return NoContent();
-        }
-
-        public override ActionResult ValidationProblem(
-            [ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
-        {
-            var apiBehaviorOptions = HttpContext.RequestServices
-                .GetRequiredService<IOptions<ApiBehaviorOptions>>()
-                .Value;
-            var result = apiBehaviorOptions.InvalidModelStateResponseFactory(ControllerContext);
-            return (ActionResult)result;
         }
     }
 }
